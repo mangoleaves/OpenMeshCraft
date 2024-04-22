01e9abb2a50fb85c38d364175b9bae8613ed0f0f
Guo Jia-Peng <gjp171499@mail.ustc.edu.cn>
2024-04-22 10:03:50 +0800

    🧪 test(Arrangements): Change Test
    Remove config in arrangements test, use default config instead.

--- test/arrangements.cpp
+++ test/arrangements.cpp
@@ -26,7 +26,6 @@ int main(int argc, char *argv[])
 	bool        output_stats  = false;
 	bool        output_result = false;
 	bool        verbose       = false;
-	bool        has_config    = false;
 
 	auto print_help = []()
 	{
@@ -50,20 +49,6 @@ int main(int argc, char *argv[])
 			output_stats = true;
 		else if (param == "-r")
 			output_result = true;
-		else if (OMC::starts_with(param, "--config="))
-		{
-			std::string config_path = param.substr(param.find_first_of('=') + 1);
-			if (boost::filesystem::is_regular_file(
-			      boost::filesystem::path(config_path)))
-			{
-				boost::property_tree::read_json(config_path, omc_test_config);
-				has_config = true;
-			}
-			else
-			{
-				OMC_ASSERT(false, "test config file error.");
-			}
-		}
 		else
 			print_help();
 	}
@@ -90,30 +75,11 @@ int main(int argc, char *argv[])
 	arrangements.addTriMeshAsInput(input_points, input_triangles);
 	arrangements.setTriMeshAsOutput(result_points, result_triangles);
 
-	if (has_config)
-	{
-		TEST_GET_CONFIG(Arrangements, TestDataSet);
-		bool set_parameter = config.get<bool>("set_parameter", false);
-		OMC::MeshArrangements_Config arr_config;
-		if (set_parameter)
-		{
-			boost::property_tree::ptree &parameters = config.get_child("parameters");
-			arr_config.tree_enlarge_ratio =
-			  parameters.get<double>("tree_enlarge_ratio");
-			arr_config.tree_adaptive_thres =
-			  parameters.get<double>("tree_adaptive_thres");
-			arr_config.tree_split_size_thres =
-			  parameters.get<size_t>("tree_split_size_thres");
-
-			arrangements.setConfig(arr_config);
-		}
-	}
-
 	OMC::MeshArrangements_Stats &stats = arrangements.stats();
 
 	auto start = OMC::Logger::elapse_reset();
 
-	arrangements.meshArrangements(false, true);
+	arrangements.meshArrangements(false, output_result);
 
 	double time = OMC::Logger::elapsed(start).count();
 
